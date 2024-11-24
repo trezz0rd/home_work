10527940d1e63a4f9ce60b67757fb11816efb337
trezz0rd <153536036+trezz0rd@users.noreply.github.com>
2024-11-24 19:59:59 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -5,6 +5,8 @@ class House:
 		print(f'Кол-во этажей - {number_of_floors}')
 
 	def go_to(self, new_floor):
+		for i in range(new_floor):
+			print('Этаж -', i)
 		if new_floor > self.number_of_floors:
 			print('Такого этажа не существует')
 		elif new_floor <= 0:
