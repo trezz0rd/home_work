f2ace15431212270262efea4ffedc84dea19375d
trezz0rd <153536036+trezz0rd@users.noreply.github.com>
2024-12-07 14:05:47 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,7 +1,11 @@
-n = int(input("Введите число от 3 до 20: "))
-result = ""
-for i in range(1, n + 1):
-    for j in range(i + 1, n + 1):
-        if (i + j) % n == 0:
-            result += str(i) + str(j)
-print("Нужный пароль для числа", n, "-", result)
+from random import randint
+number_1 = 18
+print(number_1)
+result = ''
+for i in range(1, 20):
+	for w in range(1, 20):
+		if number_1 % (i + w) == 0 and i < w:
+			result += (str(i) + str(w))
+			# print(f'{i} + {w}')
+
+print(result)
